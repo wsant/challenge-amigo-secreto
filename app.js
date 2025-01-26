77a86866ecb93eb859959f6b3a395bd8b98176b6
William Santos <william.s.s@outlook.com>
2025-01-26 13:38:52 -0300

    adicionada função para atualizar lista de amigos

--- app.js
+++ app.js
@@ -11,11 +11,24 @@ function adicionarAmigo() {
     amigos.push(campoDigiteUmNome);
     console.log(amigos);
 
-    limparCampo()
+    atualizarLista();
+    limparCampo();
 }
 
+function atualizarLista() {
+    let lista = document.querySelector('ul');
+    lista.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+        let item = document.createElement('li');
+        item.textContent = amigos[i];
+
+        lista.appendChild(item);
+    }
+ }
+ 
 
 function limparCampo() {
     let campoDigiteUmNome = document.querySelector('input');
-    campoDigiteUmNome.value = ''
+    campoDigiteUmNome.value = '';
 }
